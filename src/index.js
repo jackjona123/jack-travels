1ac37a7043f10a48df904eb40d52d9615bf237e3
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-05-30 12:16:23 -0400

    production

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//Credits Console log
+
+if (window.console) {
+        var o = navigator.userAgent.toLowerCase();
+        if (o.indexOf("chrome") > -1 || o.indexOf("firefox") > -1) {
+            window.console.log.apply(console, ["%c %c Site Built By Jack Jona %c %c  https://jackjona.com  %c ", "background: #f12626; border: 1px solid #f12626; padding:5px 0; margin:3px 0 10px 0;", "background: #ffffff; border: 1px solid #f12626; color: #f12626; padding:5px 0; margin:3px 0 10px 0;", "background: #f12626; border: 1px solid #f12626; padding:5px 0; margin:3px 0 10px 0;", "background: #ffffff; border: 1px solid #f12626; color: #f12626; padding:5px 0; margin:3px 0 10px 0;", "background: #f12626; border: 1px solid #f12626; padding:5px 0; margin:3px 0 10px 0;"])
+        } else
+            window.console.log("Site Built By Jack Jona - https://jackjona.com")
+}
+
