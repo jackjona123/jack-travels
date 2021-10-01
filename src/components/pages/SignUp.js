059ab69566b3489ccc5651168a8d29f61bea6aa1
jackjona123 <65679392+jackjona123@users.noreply.github.com>
2021-10-01 13:54:25 -0400

    update

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../../App.css';
 
-export default function SignUp() {
-  return <h1 className='sign-up'>SIGN UP</h1>;
+const SignUp = () => {
+  return (
+    <div>
+      <h1 className='sign-up'>SIGN UP</h1>
+    </div>
+  )
 }
+
+export default SignUp;
